fc926e0a0a4101dfb062a1728450292b96a807a8
ute <ute.naessens@student.kuleuven.be>
2021-02-08 14:31:52 +0100

    edit Ute

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,8 @@ public class Main {
         personList.add(new Person("Arne Hellin"));
         personList.add(new Person("Brecht Opstaele"));
         personList.add(new Person("Branko Wauters"));
-        personList.add(new Person("Robbe De Groeve"));
+        personList.add(new Person("Ute Naessens"));
+
 
         for(Person person : personList) {
             person.sayHi();
