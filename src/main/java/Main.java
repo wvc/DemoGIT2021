6e5f7485f7c8fd25055fe6fc85daddc39da54f1e
wvc <wim.vancroonenburg@gmail.com>
2021-02-08 14:06:11 +0100

    Refactoring to List<Person>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class Main {
 
     public static void main(String... args) {
-        Person person = new Person("Wim Vancroonenburg");
-        person.sayHi();
+        List<Person> personList = new ArrayList<>();
+
+        personList.add(new Person("Wim Vancroonenburg"));
+
+        for(Person person : personList) {
+            person.sayHi();
+        }
     }
 }
