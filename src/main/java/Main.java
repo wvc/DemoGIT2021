e0c91b9de4708e8cafad69147b25d978f22a4ed3
wvc <wim.vancroonenburg@gmail.com>
2021-02-08 15:20:59 +0100

    Added Pieter en Tony to Admin Group

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String... args) {
         List<Person> personList = new ArrayList<>();
 
-        personList.add(new Person("Wim Vancroonenburg"));
         personList.add(new Person("Arne Hellin"));
         personList.add(new Person("Brecht Opstaele"));
         personList.add(new Person("Toon keymeulen"));
@@ -15,7 +14,6 @@ public class Main {
         personList.add(new Person("Branko Wauters"));
         personList.add(new Person("Evelyn De Smet"));
         personList.add(new Person("Bryan Coulier"));
-        personList.add(new Person("Tony Wauters"));
         personList.add(new Person("Veerle Taleman"));
         personList.add(new Person("Tibo Verdonck"));
         personList.add(new Person("Robbe De Groeve"));
@@ -26,6 +24,7 @@ public class Main {
         personList.add(new Person("Emile Combes"));
         personList.add(new Person("Martijn Vanderschelden"));
         personList.add(new Person("Thomas Vander Beke"));
+
         for(Person person : personList) {
             person.sayHi();
         }
@@ -34,7 +33,8 @@ public class Main {
 
         Group adminGroup = new Group("Admin");
         adminGroup.addMember(new Person("Pieter Smet"));
-
+        adminGroup.addMember(new Person("Wim Vancroonenburg"));
+        adminGroup.addMember(new Person("Tony Wauters"));
 
 
 
