e9f1b67ae3e2758a5cb68df2def28aad0f7bf046
BrankoWauters <wautersbrank@gmail.com>
2021-02-08 14:20:28 +0100

    added Branko

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         personList.add(new Person("Wim Vancroonenburg"));
         personList.add(new Person("Arne Hellin"));
-
+        personList.add(new Person("Branko Wauters"));
         for(Person person : personList) {
             person.sayHi();
         }
